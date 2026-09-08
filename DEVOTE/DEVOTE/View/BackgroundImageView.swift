217df2e46b927f588cd04339832118f24ca8c9fb
Dhruv Patel <dhruvpatel913@gmail.com>
2026-09-07 20:41:32 -0700

    BackgroundImageView Added.

---
+++ DEVOTE/DEVOTE/View/BackgroundImageView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundImageView.swift
+//  DEVOTE
+//
+//  Created by Dhruv Patel on 07/09/26.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundImageView()
+}
