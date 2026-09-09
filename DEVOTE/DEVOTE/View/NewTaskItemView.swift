b40cd3002f081539b777bcbea3de129196f638be
Dhruv Patel <dhruvpatel913@gmail.com>
2026-09-08 22:52:41 -0700

    New TaskItemView Added.

---
+++ DEVOTE/DEVOTE/View/NewTaskItemView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTaskItemView.swift
+//  DEVOTE
+//
+//  Created by Dhruv Patel on 08/09/26.
+//
+
+import SwiftUI
+
+struct NewTaskItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewTaskItemView()
+}
