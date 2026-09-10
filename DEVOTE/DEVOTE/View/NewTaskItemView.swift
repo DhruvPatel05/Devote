aae12dedc65ccbc714a3da3ec2dff40e28562686
Dhruv Patel <dhruvpatel913@gmail.com>
2026-09-09 22:32:45 -0700

    NewTaskItemView main feature added.

--- DEVOTE/DEVOTE/View/NewTaskItemView.swift
+++ DEVOTE/DEVOTE/View/NewTaskItemView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct NewTaskItemView: View {
+    // MARK: - PROPERTY
+    
+    
+    
+    // MARK: -  BODY
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-#Preview {
-    NewTaskItemView()
+// MARK: -  PREVIEW
+
+struct NewTaskItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTaskItemView()
+            .previewDevice(PreviewDevice("iPhone 12 Pro"))
+            .background(Color.gray.edgesIgnoringSafeArea(.all))
+    }
 }
