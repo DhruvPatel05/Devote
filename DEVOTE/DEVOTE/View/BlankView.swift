a2e19779d56472b46cc78f68cbd492bc8aaaac04
Dhruv Patel <dhruvpatel913@gmail.com>
2026-09-12 09:32:28 -0700

    Balnkview Added for empty view.

---
+++ DEVOTE/DEVOTE/View/BlankView.swift
@@ -0,0 +1,18 @@
+//
+//  BlankView.swift
+//  DEVOTE
+//
+//  Created by Dhruv Patel on 12/09/26.
+//
+
+import SwiftUI
+
+struct BlankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlankView()
+}
