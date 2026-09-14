d27de972cf0e65ce4de0c75b067289e08a70250a
Dhruv Patel <dhruvpatel913@gmail.com>
2026-09-13 20:38:48 -0700

    BlankView Added.

--- DEVOTE/DEVOTE/View/BlankView.swift
+++ DEVOTE/DEVOTE/View/BlankView.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct BlankView: View {
+    // MARK: -  PROPERTY
+    
+    
+    
+    // MARK: -  BODY
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+        }
+        .frame(minWidth: 0,maxWidth: .infinity,minHeight: 0,maxHeight: .infinity,alignment: .center)
+        .background(Color.black)
     }
 }
 
