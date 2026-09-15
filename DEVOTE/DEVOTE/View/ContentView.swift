1f522f1315d9067ad71ab4d6102ff965d87cb70e
Dhruv Patel <dhruvpatel913@gmail.com>
2026-09-14 22:51:38 -0700

    Testing for the UI

--- DEVOTE/DEVOTE/View/ContentView.swift
+++ DEVOTE/DEVOTE/View/ContentView.swift
@@ -69,7 +69,7 @@ struct ContentView: View {
                                 showNewTaskItem = false
                             }
                         }
-                NewTaskItemView()
+                    NewTaskItemView(isShowing: $showNewTaskItem)
                 }
             }//: ZSTACK
             .onAppear(){
