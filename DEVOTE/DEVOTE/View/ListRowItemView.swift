5622e981cb72552a18fef8d6dcacca3127156170
Dhruv Patel <dhruvpatel913@gmail.com>
2026-09-20 22:55:15 -0700

    ListrowItemView Updated.

--- DEVOTE/DEVOTE/View/ListRowItemView.swift
+++ DEVOTE/DEVOTE/View/ListRowItemView.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 
 struct ListRowItemView: View {
+    
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservableObject var item: Item
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Toggle(isOn: $item.completion) {
+            Text(item.task ?? "")
+                .font(.system(.title2,design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? Color.pink : Color.primary)
+        }
     }
-}
-
-#Preview {
-    ListRowItemView()
 }
