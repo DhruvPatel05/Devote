ad2eadeb370524db6bc69bf38a17ce328fae9760
Dhruv Patel <dhruvpatel913@gmail.com>
2026-09-20 22:49:03 -0700

    ListrowItemView Added for customize item

---
+++ DEVOTE/DEVOTE/View/ListRowItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowItemView.swift
+//  DEVOTE
+//
+//  Created by Dhruv Patel on 20/09/26.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowItemView()
+}
