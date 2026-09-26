be9be367b24c0bec6e277e5171b7289f4f9be4de
Dhruv Patel <dhruvpatel913@gmail.com>
2026-09-25 21:36:19 -0700

    New Checkbox style added.

---
+++ DEVOTE/DEVOTE/Style/CheckboxStyle.swift
@@ -0,0 +1,19 @@
+//
+//  Untitled.swift
+//  DEVOTE
+//
+//  Created by Dhruv Patel on 25/09/26.
+//
+import SwiftUI
+
+struct CheckboxStyle: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CheckboxStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckboxStyle()
+    }
+}
